addca447db925b27a9b7b38fd4b8bcd1de4388a3
o.korolev <o.korolev@cian.ru>
2018-10-09 17:21:16 +0300

    init

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-from pytest_ast_transformer.ast_manager import ASTManager
-from tests.transformer import AssertTransformer
-
-
-def pytest_register_ast_transformer(ast_manager: ASTManager):
-    # pass
-    ast_manager.add_transformer(AssertTransformer())
